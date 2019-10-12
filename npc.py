7f11e0b9e9dbbe0249d991dd048e0cccaacee0ce
KarmaKamikaze <nicolaijgn@gmail.com>
2019-10-12 22:40:04 +0200

    npc stats moved to main.py

--- npc.py
+++ npc.py
@@ -49,36 +49,3 @@ class NPC:
     def stealth_check(self):
         dex_mod = int((self.dexterity - 10) / 2 + 3)
         return int(dice.roll('1d20')) + dex_mod
-
-
-# Darek Brewmont uses a d4 to roll damage with his weapon.
-darekBrewmont = NPC("Darek Brewmont", 10, 4, 10, 10, 10, 10, 10, 10, 2, 2, 0, 0)
-
-# The Giant Centipede uses a d4 to roll damage with its bite.
-giantCentipede = NPC("Giant Centipede", 13, 4, 5, 14, 12, 1, 7, 3, 4, 2, 0, 0)
-
-# The Cultists use a d6 to roll damage with its scimitar (melee) and its hand-crossbow (ranged).
-cultist = NPC("Cultist", 12, 9, 11, 12, 10, 10, 11, 10, 2, 0, 3, 0)
-
-# The Death Knight uses a d8 to roll damage with its Hellreaver.
-deathKnight = NPC("Death Knight", 15, 28, 15, 11, 13, 12, 10, 5, 5, 3, 0, 0)
-
-# The Goblins uses a d6 to roll damage with the spear and the battleaxe but a d4 for the bonestaff.
-goblin = NPC("Goblin", 15, 7, 8, 14, 10, 10, 8, 8, 1, 1, 2, 2)
-
-# The Kobold uses a d4 to roll damage with the dagger and a d6 for the fireball.
-kobold = NPC("Kobold", 12, 5, 7, 15, 9, 8, 7, 8, 4, 2, 2, 0)
-
-# The Forest Bat uses a d4 to roll damage with its bite.
-forestBat = NPC("Forest Bat", 12, 5, 7, 15, 8, 2, 12, 4, 0, 0, 0, 0)
-
-# The Skeleton uses a d6 to roll damage with its shortsword.
-skeleton = NPC("Skeleton", 13, 13, 10, 14, 15, 6, 8, 5, 4, 2, 0, 0, )
-
-# The Giant Wolf Spider uses a d6 to roll damage with its bite.
-# IMPORTANT: Set up poison damage in main code when a check is needed.
-giantWolfSpider = NPC("Giant Wolf Spider", 13, 11, 12, 16, 13, 3, 12, 4, 3, 1, 0, 0)
-
-# The Zombie uses a d6 to roll damage with its slam.
-# IMPORTANT: Set up the Undead Fortitude feature in main code when a check is needed.
-zombie = NPC("Zombie", 8, 22, 13, 6, 16, 3, 6, 5, 3, 1, 0, 0)
